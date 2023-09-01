146d83787273e9433e21b39980373bbd47fb4842
Robert Wang <wangrob@amazon.com>
2023-09-01 07:07:38 +0000

    use xgboost serverless endpoint

--- sm/xgboost/xgboost-api/xgboost_api/sm_util.py
+++ sm/xgboost/xgboost-api/xgboost_api/sm_util.py
@@ -2,30 +2,26 @@ import boto3
 import json
 
 
-def invoke_endpoint(endpoint_name, message):
+def invoke_endpoint(endpoint_name, record):
     '''Call the sagemaker (serverless) inference endpoint
     '''
-    print("invoke_endpoint: endpoint_name: %s" % (endpoint_name))
-    print("invoke_endpoint: message: %s" % (message))
+    print("invoke_endpoint: endpoint_name: %s." % (endpoint_name))
+    print("invoke_endpoint: record: %s." % (record))
 
     client = boto3.client('sagemaker-runtime')
     
-    content_type = "application/json"
-    # must specify "inputs"
-    data = {
-        "inputs": message
-    }
-    print("invoke_endpoint: data: %s" % json.dumps(data, indent=2))
+    # pyaload needs to be a byte stream so we need to encode the record
+    payload = record.encode('utf-8')
+    print("invoke_endpoint: payload's byte converted string is: %s, type: %s." % (str(payload), str(type(payload))))
+    content_type = "text/csv"
     
     ie_response = client.invoke_endpoint(
         EndpointName=endpoint_name,
-        ContentType=content_type,
-        Body=json.dumps(data)
+        Body=payload,
+        ContentType=content_type
     )
-    # ie_response['Body'] is a byte stream so we can't format it
-    print("invoke_endpoint: ie_response: %s" % (ie_response))
-
-    # ic_response['Body'] is a byte stream so we need to read and decode it
+    
+    # ic_response['Body'] is a byte stream so we need to read and decode the streamt
     response = ie_response["Body"].read().decode("utf-8")
     print("invoke_endpoint: response: %s" % json.dumps(response, indent=2))
 
